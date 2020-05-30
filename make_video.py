b0b6e952a9a5012eba53c8992a66af16d55913d0
Ed Brannin <edbrannin@gmail.com>
2020-05-30 11:02:02 -0400

    WIP: Add Python code for ffmpeg

---
+++ make_video.py
@@ -0,0 +1,49 @@
+import os
+import glob
+
+import ffmpeg
+
+MASS_VIDEOS_DIR = '/Users/edbrannin/Movies/Mass Videos'
+
+def input(f, parent_dir=MASS_VIDEOS_DIR, loop_frames=None):
+    stream = ffmpeg.input(os.path.join(parent_dir, f))
+    if loop_frames:
+        stream = ffmpeg.filter(stream, 'loop', loop=loop_frames)
+    return stream
+
+def mass_part_inputs(parent_dir):
+    # TODO: Use regex for case-insensitive match
+    files = glob.glob(os.path.join(parent_dir, '*.MP4'))
+    return [
+        input(f, parent_dir)
+        for f
+        in sorted(files)
+    ]
+
+def main():
+    mass_parts = mass_part_inputs(os.path.join(MASS_VIDEOS_DIR, '2020-05-23'))
+
+    # TODO Read from mass video
+    FRAME_RATE = 25
+
+    # Before/after
+    before_logo = input('stalbans_logo_5.mp4')
+    after_screen = input('after-message.png', loop_frames=FRAME_RATE * 30)
+
+    # Superimposed bits
+    announcements = input('announcements-background.png')
+    offertory = input('offertory-background.png')
+
+    print(before_logo)
+    # print(mass_parts)
+    print(after_screen)
+
+    print(mass_parts[-1])
+    mass_parts[-1] = ffmpeg.trim(mass_parts[-1], end=10)
+    print(mass_parts[-1])
+
+    result = ffmpeg.concat(mass_parts[-1], ffmpeg.filter([mass_parts[-1], after_screen], 'xfade'))
+    print(ffmpeg.get_args(result))
+
+if __name__ == '__main__':
+    main()
