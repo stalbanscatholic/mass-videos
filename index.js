1e5a36330c5fd79051123dd3af8a94adb9d72ec5
Ed Brannin <edbrannin@gmail.com>
2020-05-30 23:46:44 -0400

    Captions: Pentecost Update

--- index.js
+++ index.js
@@ -4,18 +4,41 @@ const moment = require('moment')
 nunjucks.configure();
 // Get massCount, massCommon from http://prayer.covert.org/tomorrow/
 const context = {
-  isodate: '2020-05-24',
-  massCount: 'Seventh Sunday of Easter',
+  isodate: '2020-05-30',
+  massCount: 'Pentecost Sunday',
   massCommon: '',
   time: '11 AM',
   priest: 'Fr. Evan Simington',
   psalm: [
-    'Psalm 27: Dominus illuminatio.',
-    'THE LORD is my light and my salvation; whom then shall I fear? * the LORD is the strength of my life; of whom then shall I be afraid?',
-    'One thing have I desired of the LORD, which I will require; * even that I may dwell in the house of the LORD all the days of my life, to behold the fair beauty of the LORD, and to visit his temple.',
-    'Therefore will I offer in his dwelling an oblation, with great gladness: * I will sing and speak praises unto the LORD. Hearken unto my voice, O LORD, when I cry unto thee; * have mercy upon me, and hear me.',
-    'My heart hath talked of thee, Seek ye my face: * Thy face, LORD, will I seek.'
+    [
+      'Psalm 104: Benedic, anima mea.',
+    ],
+    [
+      'PRAISE the LORD, O my soul:',
+      'O LORD my God, thou art become exceeding glorious;',
+      'thou art clothed with majesty and honour.',
+      'Thou deckest thyself with light as it were with a garment,',
+    ],
+    [
+      'O LORD, how manifold are thy works!',
+      'in wisdom hast thou made them all;',
+      'the earth is full of thy riches.',
+      'Praise thou the LORD, O my soul. Praise the LORD.',
+    ],
+    [
+      'These wait all upon thee,',
+      'that thou mayest give them meat in due season.',
+      'When thou givest it them, they gather it;',
+      'and when thou openest thy hand, they are filled with good.',
+    ],
+    [
+      'When thou takest away their breath, they die,',
+      'and are turned again to their dust.',
+      'When thou lettest thy breath go forth, they shall be made;',
+      'and thou shalt renew the face of the earth.',
+    ],
   ]
+  
 }
 context.mass = context.massCommon || context.massCount;
 context.date = moment(context.isodate).format('MMM D, YYYY');
@@ -29,5 +52,5 @@ const logResult = filename => {
   return result;
 };
 
-// logResult('video-captions.njk');
+logResult('video-captions.njk');
 logResult('post-body.njk');
