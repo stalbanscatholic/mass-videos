19fe64e6b7df3af1af36ead7cfa1bcda8956584e
Ed Brannin <edbrannin@gmail.com>
2020-06-13 14:36:42 -0400

    Extract TextOverlay, save transparent PNG
    Got the image I'm looking for
    
    Next up:
    - Get inputs from a file
    - Wrap long lines

--- make_captions.py
+++ make_captions.py
@@ -50,74 +50,98 @@ def rounded_rectangle(self: ImageDraw, xy, corner_radius, fill=None, outline=Non
 
 ImageDraw.rounded_rectangle = rounded_rectangle
 
+OVERLAY_BACKROUND_ALPHA = math.floor(255 * 0.7)
 
 
+class TextOverlay(object):
+    def __init__(self, text, font=None, image_size=(1920, 1080), padding=42, debug=False):
+        self.text = text
+        self.font = font
+        if self.font is None:
+            # get a font
+            self.font = ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/Ubuntu-B.ttf', 42)
+        self.image_size = image_size
+        self.padding = padding
+        self.debug = debug
 
-overlay_backround_alpha = math.floor(255 * 0.6)
+    def draw_text_image(self):
+        image = Image.new('RGBA', self.image_size, (255,255,255,0))
+        d = ImageDraw.Draw(image)
+        text_width, text_height = d.multiline_textsize(self.text, font=self.font)
+        image_width, image_height = self.image_size
+        text_upper_right_x = (image_width - text_width) / 2
 
-def get_text_overlay(text, font=None, image_size=(1920, 1080), padding=42, debug=False):
-    if font is None:
-        # get a font
-        font = ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/Ubuntu-B.ttf', 42)
+        if self.debug:
+            d.rectangle(
+                    [(text_upper_right_x, self.padding * 2), (text_upper_right_x + text_width, self.padding * 2 + text_height + 1)],
+                    outline=(255, 0, 0)
+                    )
 
-    # make a blank image for the text, initialized to transparent text color
-    txt = Image.new('RGBA', image_size, (255,255,255,0))
-
-    # get a drawing context
-    d = ImageDraw.Draw(txt)
-
-    # draw text, half opacity
-    text_width, text_height = d.multiline_textsize(text, font=font)
-    image_width, image_height = image_size
-
-    text_upper_right_x = (image_width - text_width) / 2
-
-    upper_right = (
-            text_upper_right_x - padding,
-            padding,
-            )
-    lower_left = (
-            text_upper_right_x + text_width + padding,
-            text_height + padding * 3,
-            )
-
-    rounded_rectangle(
-            d,
-            [upper_right, lower_left],
-            padding,
-            fill=(0, 0, 0, overlay_backround_alpha)
-            )
-
-    if debug:
-        d.rectangle(
-                [(text_upper_right_x, padding * 2), (text_upper_right_x + text_width, padding * 2 + text_height + 1)],
-                outline=(255, 0, 0)
+        d.multiline_text(
+                (text_upper_right_x, self.padding * 2),
+                self.text,
+                font=self.font,
+                fill=(255,255,255,255),
+                align="center",
                 )
 
-    d.multiline_text(
-            (text_upper_right_x, padding * 2),
-            text,
-            font=font,
-            fill=(255,255,255,255),
-            align="center",
-            )
+        return image;
+        
 
-    return txt
+    def draw_background(self):
+        image = Image.new('RGBA', self.image_size, (255,255,255,0))
+        d = ImageDraw.Draw(image)
+        text_width, text_height = d.multiline_textsize(self.text, font=self.font)
+        image_width, image_height = self.image_size
+        text_upper_right_x = (image_width - text_width) / 2
 
-    # result = Image.alpha_composite(base, txt)
-    # return result
+        upper_right = (
+                text_upper_right_x - self.padding,
+                self.padding,
+                )
+        lower_left = (
+                text_upper_right_x + text_width + self.padding,
+                text_height + self.padding * 3,
+                )
 
+        rounded_rectangle(
+                d,
+                [upper_right, lower_left],
+                self.padding,
+                fill=(0, 0, 0, OVERLAY_BACKROUND_ALPHA)
+                )
+
+        # return image.filter(ImageFilter.BoxBlur(2))
+        return image.filter(ImageFilter.GaussianBlur(5))
+        
+    def get_overlay(self):
+        return Image.alpha_composite(
+                self.draw_background(),
+                self.draw_text_image(),
+                )
+
+def get_text_overlay(text, font=None, image_size=(1920, 1080), padding=42, debug=False):
+    overlay = TextOverlay(text, font=None, image_size=(1920, 1080), padding=42, debug=False)
+    return overlay.get_overlay()
+
+TEXT = """
+In union, dear Lord, with all the faithful at every altar of thy Church
+where the blessed Body and Blood are being offered to the Father,
+I desire to offer thee praise and thanksgiving. I believe thou
+art truly present in the Most Holy Sacrament.
+
+And since I cannot now receive thee sacramentally, I beseech thee to
+come spiritually into my heart. I unite myself unto thee, and embrace thee
+with all the affections of my soul. Let me never be separated from thee.
+Let me live and die in thy love.
+
+Amen.
+""".strip()
 
 def main():
-    # get an image
-    base = Image.open('kitten.jpg').convert('RGBA')
-
-    text = "\n".join(["Hello", "World"])
-    txt = get_text_overlay(text)
-
-    out = Image.alpha_composite(base, txt)
-
-    out.show()
+    text_image = get_text_overlay(TEXT)
+    text_image.show()
+    text_image.save('caption.png')
 
 if __name__ == '__main__':
     main()
